a0c0d835ee2ca5ea9e032e980788d812c62add86
Hèctor Morales Carnicé <hector.morales.carnice@gmail.com>
2024-03-14 14:30:28 +0000

    chore: use pseudo-command pattern

--- plotline-plugin/src/experience.rs
+++ plotline-plugin/src/experience.rs
@@ -1,13 +1,16 @@
 use crate::{Error, FlavoredPlugin, Plugin, PluginKind, Result};
-use plotline::experience::{Experience, ExperiencedEvent};
+use protobuf::Message;
+use plotline::experience::ExperiencedEvent;
+use plotline_proto::plugin::{OnSaveExperienceInput, OnSaveExperienceOutput};
 
-/// An OnSaveExperience is a plugin that is executed before saving an
-/// [Experience].
-pub struct OnSaveExperience<'a> {
+/// An OnSaveExperiencePlugin is a plugin that is executed before saving
+/// an [Experience], determining if the experience is suitable to be saved
+/// or not.
+pub struct OnSaveExperiencePlugin<'a> {
     plugin: &'a Box<dyn Plugin>,
 }
 
-impl<'a> TryFrom<&'a Box<dyn Plugin>> for OnSaveExperience<'a> {
+impl<'a> TryFrom<&'a Box<dyn Plugin>> for OnSaveExperiencePlugin<'a> {
     type Error = Error;
 
     fn try_from(plugin: &'a Box<dyn Plugin>) -> Result<Self> {
@@ -19,18 +22,57 @@ impl<'a> TryFrom<&'a Box<dyn Plugin>> for OnSaveExperience<'a> {
     }
 }
 
-impl<'a> FlavoredPlugin<'a> for OnSaveExperience<'a> {
+impl<'a> FlavoredPlugin<'a> for OnSaveExperiencePlugin<'a> {
     fn kind() -> PluginKind {
         PluginKind::OnSaveExperience
     }
 }
 
-impl<'a> OnSaveExperience<'a> {
-    pub fn execute<'b, Intv>(
-        &self,
-        subject: ExperiencedEvent<'b, Intv>,
-        timeline: &[ExperiencedEvent<'b, Intv>],
-    ) -> Result<()> {
-        todo!()
+impl<'a> OnSaveExperiencePlugin<'a> {
+    pub fn with_subject<'b, Intv>(&self, subject: &'b ExperiencedEvent<'b, Intv>) ->  OnSaveExperience<'a, 'b, Intv> {
+        OnSaveExperience {
+            plugin: self.plugin,
+            subject,
+            timeline: Default::default(),
+            result: Err("not executed".to_string()),
+        }
+    }
+}
+
+/// OnSaveExperience is the [OnSaveExperiencePlugin]'s command.
+pub struct OnSaveExperience<'a, 'b, Intv> {
+    plugin: &'a Box<dyn Plugin>,
+    subject: &'b ExperiencedEvent<'b, Intv>,
+    timeline: &'a [&'b ExperiencedEvent<'b, Intv>],
+    result: std::result::Result<(), String>,
+}
+
+impl<'a, 'b, Intv> OnSaveExperience<'a, 'b, Intv> {
+    pub fn with_timeline(mut self, timeline: &'a [&'b ExperiencedEvent<'b, Intv>]) -> Self {
+        self.timeline = timeline;
+        self
+    }
+
+    pub fn execute(mut self) -> Self {
+        self.result = self.run();
+        self
+    }
+
+    pub fn result(self) -> std::result::Result<(), String> {
+        self.result
+    } 
+
+    fn run(&self) -> std::result::Result<(), String> {
+        let input = OnSaveExperienceInput::default();
+        let input = input.write_to_bytes().map_err(|err| err.to_string())?;
+        
+        let output = self.plugin.run("main", &input)?;
+        let output = OnSaveExperienceOutput::parse_from_bytes(&output).map_err(|err| err.to_string())?;
+        
+        if !output.error.is_empty() {
+            return Err(output.error);
+        }
+
+        Ok(())
     }
 }
