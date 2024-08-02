0d9bfd50ab99457de95ad77a29a2951ac3c3d611
hectormrc <thehector1593@gmail.com>
2024-08-02 10:16:29 +0200

    doc: cargo doc for schema trait

--- alvidir/src/schema/mod.rs
+++ alvidir/src/schema/mod.rs
@@ -1,2 +1,5 @@
-/// Represents an specific structure that must be satisfied.
-pub trait Schema {}
+/// Defines the structure and validation rules for data within the system.
+pub trait Schema {
+    /// The type over which the schema applies.
+    type Document;
+}
