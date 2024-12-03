97acb57d030552226168cf291be088841aafa571
hector <hector.morales@veecle.io>
2024-12-03 19:04:00 +0100

    chore: move logic to repo impl

--- alvidir-cli/src/main.rs
+++ alvidir-cli/src/main.rs
@@ -5,10 +5,10 @@ use std::{
     sync::{Arc, LazyLock},
 };
 
-use alvidir::{document::lazy::LazyDocument, graph::Graph};
+use alvidir::graph::Graph;
 use alvidir_cli::{repository::LocalDocumentRepository, CliCommand};
+use anyhow::Result;
 use clap::Parser;
-use ignore::Walk;
 use regex::Regex;
 use tracing::Level;
 
@@ -51,53 +51,7 @@ struct Cli {
     pattern: String,
 }
 
-impl Cli {
-    fn documents(&self) -> impl Iterator<Item = PathBuf> + '_ {
-        Walk::new(&self.context)
-            .filter_map(move |entry| {
-                if let Err(err) = &entry {
-                    tracing::error!(
-                        error = err.to_string(),
-                        context = self.context.to_string_lossy().to_string(),
-                        "walking base directory"
-                    );
-                }
-
-                entry.ok()
-            })
-            .filter({
-                let pattern = Regex::new(&self.pattern)
-                    .expect("pattern should be a valid regular expression");
-
-                move |entry| {
-                    let matches = pattern.is_match(&entry.file_name().to_string_lossy());
-                    tracing::debug!(path = entry.path().to_string_lossy().to_string(), matches);
-
-                    matches
-                }
-            })
-            .filter_map(move |entry| {
-                let path = entry
-                    .path()
-                    .strip_prefix(&self.context)
-                    .map(ToOwned::to_owned);
-
-                if let Err(err) = &path {
-                    tracing::error!(
-                        error = err.to_string(),
-                        path = entry.path().to_string_lossy().to_string(),
-                        context = self.context.to_string_lossy().to_string(),
-                        "stripping context from path"
-                    );
-                }
-
-                path.ok()
-            })
-    }
-}
-
-#[tokio::main]
-async fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
     tracing_subscriber::fmt()
@@ -109,7 +63,10 @@ async fn main() {
 
     let document_repo = Arc::new(LocalDocumentRepository {
         context: &args.context,
+        pattern: Regex::new(&args.pattern).expect("pattern should be a valid regular expression"),
     });
 
-    let _graph_app = Graph::from_iter(args.documents().map(LazyDocument::builder(document_repo)));
+    let _graph_app = Graph::from_iter(document_repo.all());
+
+    Ok(())
 }
--- alvidir-cli/src/repository.rs
+++ alvidir-cli/src/repository.rs
@@ -1,9 +1,15 @@
 use std::{
     fs::File,
     path::{Path, PathBuf},
+    sync::Arc,
 };
 
-use alvidir::{document::DocumentRepository, id::Identify};
+use alvidir::{
+    document::{lazy::LazyDocument, DocumentRepository},
+    id::Identify,
+};
+use ignore::Walk;
+use regex::Regex;
 
 /// A file-system document.
 pub struct Document {
@@ -19,10 +25,12 @@ impl Identify for Document {
     }
 }
 
-/// Implements the [DocumentRepository] trait taking as datasource the given local directory.
+/// Implements the [`DocumentRepository`] trait taking as datasource the given local directory.
 pub struct LocalDocumentRepository<'a> {
     /// The base path in which the repository has to look up for files.
     pub context: &'a Path,
+    /// The filename pattern.
+    pub pattern: Regex,
 }
 
 impl<'a> DocumentRepository for LocalDocumentRepository<'a> {
@@ -30,5 +38,46 @@ impl<'a> DocumentRepository for LocalDocumentRepository<'a> {
 
     fn find_by_id(&self, _id: &<Self::Document as Identify>::Id) -> Option<Self::Document> {
         unimplemented!()
+    }
+}
+
+impl<'a> LocalDocumentRepository<'a> {
+    pub fn all(self: &Arc<Self>) -> impl Iterator<Item = LazyDocument<Self>> + '_ {
+        Walk::new(&self.context)
+            .filter_map(move |entry| {
+                if let Err(err) = &entry {
+                    tracing::error!(
+                        error = err.to_string(),
+                        context = self.context.to_string_lossy().to_string(),
+                        "walking base directory"
+                    );
+                }
+
+                entry.ok()
+            })
+            .filter(move |entry| {
+                let matches = self.pattern.is_match(&entry.file_name().to_string_lossy());
+                tracing::debug!(path = entry.path().to_string_lossy().to_string(), matches);
+
+                matches
+            })
+            .filter_map(move |entry| {
+                let path = entry
+                    .path()
+                    .strip_prefix(&self.context)
+                    .map(ToOwned::to_owned);
+
+                if let Err(err) = &path {
+                    tracing::error!(
+                        error = err.to_string(),
+                        path = entry.path().to_string_lossy().to_string(),
+                        context = self.context.to_string_lossy().to_string(),
+                        "stripping context from path"
+                    );
+                }
+
+                path.ok()
+            })
+            .map(LazyDocument::builder(self.clone()))
     }
 }
